bf13dc3e3507e944166751cf968a6752391bd81d
Yanis Lukes <Pendrokar@gmail.com>
2025-01-04 22:19:45 +0200

    new files sample_caching

---
+++ test_tts_styletts_kokoro.py
@@ -0,0 +1,32 @@
+import os
+from gradio_client import Client, file
+
+client = Client("hexgrad/kokoro", hf_token=os.getenv('HF_TOKEN'))
+# endpoints = client.view_api(all_endpoints=True, print_info=False, return_format='dict')
+# print(endpoints)
+result = client.predict(
+		text='"I hate it when people lie to me."',
+		voice="af",
+		ps=None,
+		speed=1,
+		trim=0,
+		use_gpu=False,
+		# *[
+        #     "Oh, hello there!!",
+		# 	"af", #voice
+		# 	None, #ps
+		# 	1, #speed
+		# 	3000, #trim
+		# 	False, #use_gpu; fast enough with multithreaded with CPU
+		# ],
+		api_name="/generate"
+)
+
+print(result)
+
+# text="Oh, hello there!!",
+# voice="af",
+# ps=None,
+# speed=1,
+# trim=3000,
+# use_gpu=False,
