ff1cd037499ecb08f39795e66a907c679d5d1ca1
Yanis L <Pendrokar@users.noreply.huggingface.co>
2025-01-16 15:18:09 +0000

    default_concurrency_limit: 50=>4

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
 from app.ui import app
 
 if __name__ == "__main__":
-    app.queue(default_concurrency_limit=50, api_open=False).launch(show_api=False)
+    app.queue(default_concurrency_limit=4, api_open=False).launch(show_api=False)
