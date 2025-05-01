ba6c0cbd389ba62509dafbe5313759f5cce46665
Yanis Lukes <Pendrokar@gmail.com>
2025-05-01 22:40:03 +0300

    parler multi & dia test scripts

---
+++ test_tts_dia.py
@@ -0,0 +1,46 @@
+import os
+from test_overrides import _get_param_examples, _override_params
+from gradio_client import Client, file
+
+model = "nari-labs/Dia-1.6B"
+client = Client(model, hf_token=os.getenv('HF_TOKEN'))
+endpoints = client.view_api(all_endpoints=True, print_info=False, return_format='dict')
+
+api_name = '/generate_audio'
+fn_index = None
+end_parameters = None
+text = '[2] This is what my voice sounds like. \n[2] \n[2]'
+
+end_parameters = _get_param_examples(
+	endpoints['named_endpoints'][api_name]['parameters']
+)
+print(end_parameters)
+
+
+# override some or all default parameters
+space_inputs = _override_params(end_parameters, model)
+
+if(type(space_inputs) == dict):
+	space_inputs['text_input'] = text
+	result = client.predict(
+		**space_inputs,
+		api_name=api_name
+	)
+else:
+	space_inputs[0] = text
+	result = client.predict(
+		*space_inputs,
+		api_name=api_name
+	)
+	# space_inputs = {str(i): value for i, value in enumerate(space_inputs)}
+
+print(space_inputs)
+# print(*space_inputs)
+# print(**space_inputs)
+
+# result = client.predict(
+# 	**space_inputs,
+# 	api_name=api_name,
+#     fn_index=fn_index
+# )
+print(result)
